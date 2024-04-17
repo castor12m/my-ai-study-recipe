2fd0b842497cd8aec209535ca7ec92c90bfc0186
castor_win <castor@naraspace>
2024-04-18 02:20:47 +0900

    02-step requirements.txt 파일 추가

--- 02-step-gradio/install.sh
+++ 02-step-gradio/install.sh
@@ -1,7 +1,7 @@
 
 python -m venv venv
 . ./venv/bin/activate
-#pip install -r ./requirements.txt
+pip install -r ./requirements.txt
 
 #pip install tomlkit==0.12.2
 pip install gradio # 그라디오 라이브러리를 설치합니다.
